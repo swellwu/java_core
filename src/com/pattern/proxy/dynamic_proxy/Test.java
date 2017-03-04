46c6488799ad0a102f59eb0c4c68259a6d583fcb
swellwu <heng_0577@163.com>
2017-03-05 04:13:27 +0800

    动态代理类保存

--- src/com/pattern/proxy/dynamic_proxy/Test.java
+++ src/com/pattern/proxy/dynamic_proxy/Test.java
@@ -7,9 +7,15 @@ import com.pattern.proxy.RealSubject;
  * Created by wuxinjian on 2017/2/21.
  */
 public class Test {
+
+    static {
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+    }
+
     public static void main(String[] args) {
         RealSubject realSubject = new RealSubject();
         ISubject subject = (ISubject)SubjectInvocationHandler.getProxyInstanceFactory(realSubject);
         subject.request();
+        System.out.println(subject.getClass());
     }
 }
